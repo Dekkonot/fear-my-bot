f9d6abb1891cd8f906ae6b0bd15b14aaeafef354
Dekkonot <4625546+Dekkonot@users.noreply.github.com>
2020-12-04 20:15:59 -0800

    Add init to internal command_files

---
+++ libs/commands/command_files/init.lua
@@ -0,0 +1,31 @@
+local pathjoin = require("pathjoin")
+local pathJoin = pathjoin.pathJoin
+
+local BASE_DIRECTORY = pathJoin("commands", "command_files")
+
+local commands = {
+    ["internal"] = {
+        "ping", "reload_command", "client_info", "purge_cache", "exec",
+    },
+    ["management"] = {
+        "block_permissions", "give_permissions", "reset_permissions", "set_prefix",
+    },
+    ["misc"] = {
+        "help",
+    }
+}
+
+local commandData = {}
+
+for category, commandList in pairs(commands) do
+    for _, commandName in ipairs(commandList) do
+        local path = pathJoin(BASE_DIRECTORY, category, commandName)
+        local command = require(path)
+        if commandData[command.name] then
+            error("could not load base commands: duplicate command name '" .. command .. "'", 2)
+        end
+        commandData[command.name] = {command, path}
+    end
+end
+
+return commandData
