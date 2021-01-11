544a8fee2b98227aac254b82514797f4cb7fbb09
Dekkonot <4625546+Dekkonot@users.noreply.github.com>
2021-01-10 23:05:46 -0800

    Update patch number because I forgot like a fool lol

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = "Dekkonot/fear-my-bot",
-    version = "0.2.0",
+    version = "0.2.1",
     dependencies = {
         "SinisterRectus/discordia@2.8.4",
         "luvit/json@2.5.2",
