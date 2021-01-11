a75a774c182c2cec44bc1b69b7aa983c23df1b10
Dekkonot <4625546+Dekkonot@users.noreply.github.com>
2021-01-10 22:07:46 -0800

    Update package manifest

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
-    name = "dekkonot/fear-my-bot",
-    version = "0.0.1",
+    name = "Dekkonot/fear-my-bot",
+    version = "0.1.0",
     dependencies = {
         "SinisterRectus/discordia@2.8.4",
         "luvit/json@2.5.2",
