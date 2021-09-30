505a090a94a4c8962cc70d0302e04a69f282290c
Dekkonot <4625546+Dekkonot@users.noreply.github.com>
2021-09-29 22:04:37 -0700

    V0.2.2

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = "Dekkonot/fear-my-bot",
-    version = "0.2.1",
+    version = "0.2.2",
     dependencies = {
         "SinisterRectus/discordia@2.8.4",
         "luvit/json@2.5.2",
